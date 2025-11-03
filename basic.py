4e6863fa6e27d9c551806e2257f4fa0409cdea54
Isaac Restrick <isaac.restrick@gmail.com>
2025-11-03 14:24:02 -0500

    initial

---
+++ basic.py
@@ -0,0 +1,22 @@
+from vllm import LLM, SamplingParams
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+prompts = [
+    "hi, my name is",
+    "The president of America is"
+]
+
+sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
+
+def main():
+    llm = LLM(model="TinyLlama/TinyLlama-1.1B-Chat-v1.0")
+    outputs = llm.generate(prompts, sampling_params)
+    for output in outputs:
+        print(output.prompt)
+        print(output.outputs[0].text)
+
+if __name__ == "__main__":
+    main()
